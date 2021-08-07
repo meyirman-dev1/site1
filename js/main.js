39259b34dca571830ca3fe7d2f40f9b8b8bc6e3c
meyirman-dev1 <88575156+meyirman-dev1@users.noreply.github.com>
2021-08-08 05:17:59 +0600

    site

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+
+   $('.reviews-slider').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      autoplay: true,
+      dots: true,
+      arrows: false,
+      responsive: [
+         {
+           breakpoint: 1106,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+           }
+         },
+      ]
+   });
+
+
+   $('.btn__menu').on('click', function(){
+      $('.menu__list').slideToggle();
+   });
+
+});
